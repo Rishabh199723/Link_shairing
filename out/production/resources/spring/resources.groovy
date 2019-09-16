631f29e7e291d15e2775fbfb83f82d0ad2f76081
Rishabh199723 <rishabh.gupta@rxlogix.com>
2019-09-16 12:35:17 +0530

    changes

--- out/production/resources/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-import project.UserPasswordEncoderListener
-//import demo.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-//    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-
-}
